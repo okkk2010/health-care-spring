dc77e5ccb88be6f86f641077c71e1ddba7e510b3
okkk2010 <okkk2010@naver.com>
2026-02-16 00:13:47 +0900

    init project

---
+++ src/main/java/inhatc/cse/inhive/InhiveApplication.java
@@ -0,0 +1,13 @@
+package inhatc.cse.inhive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InhiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InhiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inhatc/cse/inhive/InhiveApplicationTests.java
@@ -0,0 +1,13 @@
+package inhatc.cse.inhive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InhiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
